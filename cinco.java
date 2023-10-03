b5fb55fcfe8db70915dc401bd88ea65399fcf87c
Gabriel Ferreira <53950364+thegabriew@users.noreply.github.com>
2023-10-03 14:39:34 -0300

    Add files via upload

---
+++ cinco.java
@@ -0,0 +1,16 @@
+public class cinco {
+    public static void main (String[]args){
+        int a =0;
+         int b=1;
+         
+        for (int i=1;i<=12;i++){
+              System.out.println(b);
+
+           int aux = a+b;
+            a =b;
+            b =aux;
+         
+        }
+        
+    }
+}
---
+++ quatro.java
@@ -0,0 +1,18 @@
+public class quatro {
+    public static void main (String [] args){
+
+        int a = 18;
+
+            if(a%a==0&&a%1==0&&a%a==1){
+
+                System.out.println("Este número é primo");
+
+            }
+
+            else{
+                System.out.println("Este número não é primo");
+            }
+    }
+    
+}
+
